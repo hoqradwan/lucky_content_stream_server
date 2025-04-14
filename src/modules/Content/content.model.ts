9f047793d31b9975ece7a12a0e2618c01b80c24f
hoqradwan <hmdradwanul@gmail.com>
2025-04-14 10:15:40 +0600

    content model

---
+++ src/modules/Content/content.model.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+const contentSchema = new mongoose.Schema({
+    title : { type: String, required: true },
+    genre: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Genre' }],                              
+    release_date: { type: Date, default : Date.now() },                          
+    description: { type: String },                          
+    rating: { type: Number , default : 0},                             
+    category: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true }], // movie, tv show, etc.
+    thumbnail: { type: String, default: "" },                            
+    content_url: { type: String , default : ""},                          
+    language: { type: String, default : "" },                            
+    status: { type: String, enum: ['Active', 'Inactive'], default: 'Active' },  // Content status
+});
+
+const Content = mongoose.model('Content', contentSchema);
+module.exports = Content;
